7b06b199be3706a546b005ad5d3f8bc89c1967f9
cauan <cauandeoliveiracosta@gmail.com>
2023-12-13 10:11:35 -0300

    fix: fix when computer selection when it is a tie

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-/*
-Play a number of games, printing the winner of each game and overall winner at the end
-*/
-function game(numberOfRounds = 5) {
-  const possibleChoices = ["rock", "paper", "scissors"];
-  const scores = [];
-
-  // Play the game for the number of rounds chosen
-  for (let i = 0; i <= numberOfRounds - 1; i++) {
-    let playerSelection = prompt("Make a choice: Rock, paper or scissors?");
-    let computerSelection = getRandomItem(possibleChoices);
-    let isPlayerWinner = false;
-
-    // If its an invalid choice, ask the player to select again
-    let isValidSelection = possibleChoices.includes(
-      playerSelection.toLocaleLowerCase()
-    );
-    while (!isValidSelection) {
-      playerSelection = prompt(
-        "Invalid choice! Please choice between rock, paper or scissors"
-      );
-
-      isValidSelection = possibleChoices.includes(
-        playerSelection.toLocaleLowerCase()
-      );
-    }
-
-    // If it is a tie, ask the player to select again
-    while (playerSelection === computerSelection) {
-      console.log("It's a tie, play again!");
-      playerSelection = prompt("Make a choice: Rock, paper or scissors?");
-      computerSelection = getRandomItem();
-    }
-
-    isPlayerWinner = getRoundWinner(playerSelection, computerSelection);
-    scores.push(isPlayerWinner);
-
-    if (isPlayerWinner) {
-      console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    } else {
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-    }
-  }
-
-  const winner = getWinner(scores, numberOfRounds);
-  const message = `Overall winner is ${winner.toUpperCase()}! Congratulations!`;
-
-  console.log(message);
-}
-
 function getWinner(scores, numberOfRounds) {
   const winningScore = Math.floor(numberOfRounds / 2) + 1;
   const playerScore = scores.reduce((a, b) => a + b, 0);
@@ -80,4 +30,54 @@ function getRoundWinner(playerSelection, computerSelection) {
 function getRandomItem(possibleChoices) {
   const randomIndex = Math.floor(Math.random() * possibleChoices.length);
   return possibleChoices[randomIndex];
+}
+
+/*
+Play a number of games, printing the winner of each game and overall winner at the end
+*/
+function game(numberOfRounds = 5) {
+  const possibleChoices = ["rock", "paper", "scissors"];
+  const scores = [];
+
+  // Play the game for the number of rounds chosen
+  for (let i = 0; i <= numberOfRounds - 1; i++) {
+    let playerSelection = prompt("Make a choice: Rock, paper or scissors?");
+    let computerSelection = getRandomItem(possibleChoices);
+    let isPlayerWinner = false;
+
+    // If its an invalid choice, ask the player to select again
+    let isValidSelection = possibleChoices.includes(
+      playerSelection.toLocaleLowerCase()
+    );
+    while (!isValidSelection) {
+      playerSelection = prompt(
+        "Invalid choice! Please choice between rock, paper or scissors"
+      );
+
+      isValidSelection = possibleChoices.includes(
+        playerSelection.toLocaleLowerCase()
+      );
+    }
+
+    // If it is a tie, ask the player to select again
+    while (playerSelection === computerSelection) {
+      console.log("It's a tie, play again!");
+      playerSelection = prompt("Make a choice: Rock, paper or scissors?");
+      computerSelection = getRandomItem(possibleChoices);
+    }
+
+    isPlayerWinner = getRoundWinner(playerSelection, computerSelection);
+    scores.push(isPlayerWinner);
+
+    if (isPlayerWinner) {
+      console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    } else {
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+    }
+  }
+
+  const winner = getWinner(scores, numberOfRounds);
+  const message = `Overall winner is ${winner.toUpperCase()}! Congratulations!`;
+
+  console.log(message);
 }
